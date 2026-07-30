efcf875e982efa26aa7b697b2a21e222eb8deb70
usamaahma <usamaahmad087@gmail.com>
2026-07-30 17:20:20 +0500

    changed User

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
-import User from "@/models/User";
+import User from "@/models/user";
 
 // GET: User ki details fetch karna
 export async function GET(req) {
